8d29703c36383ba9f296ff5268f8ebe9a53aefbe
Kumaran <senthamiz.kumaran@accenture.com>
2019-01-31 16:59:15 +0530

    header

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
             {
                 test: /\.s?css$/,
                 use: ['style-loader','css-loader','sass-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: ['file-loader']
             }
         ]
     },
