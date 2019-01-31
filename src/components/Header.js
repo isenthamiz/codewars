983ce5b639e9e1ecc7a9e5e02fa1427f17faa306
Kumaran <senthamiz.kumaran@accenture.com>
2019-01-31 10:29:22 +0530

    codewars application

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div className="fixed-header shadow">
+                <header id="header">Welcome</header>
+            </div>
+        )
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname,'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style-loader','css-loader','sass-loader']
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname,'public'),
+        historyApiFallback: true,
+        port: 3001
+    },
+    mode: 'development'
+}
